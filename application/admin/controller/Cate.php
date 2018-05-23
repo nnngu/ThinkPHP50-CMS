195582831f2fe2f3023fe6a36a1567e23d2b61b8
nnngu <654015592@qq.com>
2018-05-23 23:42:02 +0800

    无限级分类的删除，级联删除

--- application/admin/controller/Cate.php
+++ application/admin/controller/Cate.php
@@ -5,10 +5,14 @@ namespace app\admin\controller;
 
 class Cate extends Common
 {
+    protected $beforeActionList = [
+        'delSonCate' => ['only' => 'del'],
+    ];
+
     public function lst()
     {
         $cate = new \app\admin\model\Cate();
-        $cateres = $cate->catetree();
+        $cateres = $cate->cateTree();
         $this->assign('cateres', $cateres);
         return view();
     }
@@ -25,8 +29,28 @@ class Cate extends Common
                 $this->error('添加栏目失败！');
             }
         }
-        $cateres = $cate->catetree();
+        $cateres = $cate->cateTree();
         $this->assign('cateres', $cateres);
         return view();
+    }
+
+    public function del()
+    {
+        $del = db('cate')->delete(input('id'));
+        if ($del) {
+            $this->success('删除栏目成功！', url('lst'));
+        } else {
+            $this->error('删除栏目失败！');
+        }
+    }
+
+    public function delSonCate()
+    {
+        $cateId = input('id');
+        $cateModel = new \app\admin\model\Cate();
+        $sonIds = $cateModel->getChildrenId($cateId);
+        if ($sonIds) {
+            db('cate')->delete($sonIds);
+        }
     }
 }
--- application/admin/model/Cate.php
+++ application/admin/model/Cate.php
@@ -6,7 +6,7 @@ use think\Model;
 
 class Cate extends Model
 {
-    public function catetree()
+    public function cateTree()
     {
         $cateres = $this->select();
         return $this->sort($cateres);
@@ -20,6 +20,24 @@ class Cate extends Model
                 $v['level'] = $level;
                 $arr[] = $v;
                 $this->sort($data, $v['id'], $level + 1);
+            }
+        }
+        return $arr;
+    }
+
+    public function getChildrenId($cateId)
+    {
+        $cateRes = $this->select();
+        return $this->_getChildrenId($cateRes, $cateId);
+    }
+
+    public function _getChildrenId($cateRes, $cateId)
+    {
+        static $arr = array();
+        foreach ($cateRes as $k => $v) {
+            if ($v['pid'] == $cateId) {
+                $arr[] = $v['id'];
+                $this->_getChildrenId($cateRes, $v['id']);
             }
         }
         return $arr;
