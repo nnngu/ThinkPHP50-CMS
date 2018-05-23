fff37c5101b7b04c63572b047cfe49e937393f52
nnngu <654015592@qq.com>
2018-05-23 19:45:48 +0800

    在控制器里 实现栏目添加的方法

--- application/admin/controller/Cate.php
+++ application/admin/controller/Cate.php
@@ -7,7 +7,6 @@ class Cate extends Common
 {
     public function lst()
     {
-        $cate = new \app\admin\model\Cate();
         $adminres = db('admin')->select();
         $this->assign('adminres', $adminres);
         return view();
@@ -15,6 +14,18 @@ class Cate extends Common
 
     public function add()
     {
+        $cate = new \app\admin\model\Cate();
+        if (request()->isPost()) {
+            $cate->data(input('post.'));
+            $add = $cate->save();
+            if ($add) {
+                $this->success('添加栏目成功！', url('lst'));
+            } else {
+                $this->error('添加栏目失败！');
+            }
+        }
+        $cateres = $cate->select();
+        $this->assign('cateres', $cateres);
         return view();
     }
 }
