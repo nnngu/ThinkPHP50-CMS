2d59faea1ff5435f046c3f857d1e84dfebef87f9
nnngu <654015592@qq.com>
2018-05-25 17:11:13 +0800

    添加 Conf 控制器，以及它的模板文件

---
+++ application/admin/controller/Conf.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\admin\controller;
+
+class Conf extends Common
+{
+    public function lst()
+    {
+        return view();
+    }
+
+    public function add()
+    {
+        return view();
+    }
+
+    public function edit()
+    {
+        return view();
+    }
+}
