307e4bfbc549984d59a555ec4b01c232b59cfa5e
nnngu <654015592@qq.com>
2018-05-25 17:41:37 +0800

    Conf 控制器的 add 和 lst 方法，即配置的添加和展示

--- application/admin/controller/Conf.php
+++ application/admin/controller/Conf.php
@@ -6,11 +6,23 @@ class Conf extends Common
 {
     public function lst()
     {
+        $conf = new \app\admin\model\Conf();
+        $confRes = $conf->paginate(5);
+        $this->assign('confRes', $confRes);
         return view();
     }
 
     public function add()
     {
+        if (request()->isPost()) {
+            $data = input('post.');
+            $conf = new \app\admin\model\Conf();
+            if ($conf->save($data)) {
+                $this->success('添加配置成功！', url('lst'));
+            } else {
+                $this->error('添加配置失败！');
+            }
+        }
         return view();
     }
 
