8686154c1ca68eeb22a7ac7276a74868103959d3
nnngu <654015592@qq.com>
2018-05-27 00:46:47 +0800

    用户组(AuthGroup) 的添加和显示

--- application/admin/controller/AuthGroup.php
+++ application/admin/controller/AuthGroup.php
@@ -7,11 +7,23 @@ class AuthGroup extends Common
 {
     public function lst()
     {
+        $authGroupRes = \app\admin\model\AuthGroup::paginate(3);
+        $this->assign('authGroupRes', $authGroupRes);
         return view();
     }
 
     public function add()
     {
+        if (request()->isPost()) {
+            $data = input('post.');
+            $add = db('auth_group')->insert($data);
+            if ($add) {
+                $this->success('添加用户组成功！', url('lst'));
+            } else {
+                $this->error('添加用户组失败！');
+            }
+            return;
+        }
         return view();
     }
 }
