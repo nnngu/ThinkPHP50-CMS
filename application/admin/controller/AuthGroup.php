f8ccc5625a6e869f9980377e19c45c12923d6adb
nnngu <654015592@qq.com>
2018-05-26 19:30:50 +0800

    用户组管理的界面

---
+++ application/admin/controller/AuthGroup.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\admin\controller;
+
+
+class AuthGroup extends Common
+{
+    public function lst()
+    {
+        return view();
+    }
+
+    public function add()
+    {
+        return view();
+    }
+}
