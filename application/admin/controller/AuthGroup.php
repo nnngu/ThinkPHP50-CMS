9fe8d1d4c1ab3f798f443d4eeeaf503100b7a09d
nnngu <654015592@qq.com>
2018-05-27 02:13:05 +0800

    用户组(AuthGroup) 的删除和修改

--- application/admin/controller/AuthGroup.php
+++ application/admin/controller/AuthGroup.php
@@ -7,7 +7,7 @@ class AuthGroup extends Common
 {
     public function lst()
     {
-        $authGroupRes = \app\admin\model\AuthGroup::paginate(3);
+        $authGroupRes = \app\admin\model\AuthGroup::paginate(5);
         $this->assign('authGroupRes', $authGroupRes);
         return view();
     }
@@ -16,6 +16,12 @@ class AuthGroup extends Common
     {
         if (request()->isPost()) {
             $data = input('post.');
+            if (array_key_exists('status', $data)) {
+                $data['status'] = 1;
+            } else {
+                $data['status'] = 0;
+            }
+
             $add = db('auth_group')->insert($data);
             if ($add) {
                 $this->success('添加用户组成功！', url('lst'));
@@ -25,5 +31,39 @@ class AuthGroup extends Common
             return;
         }
         return view();
+    }
+
+    public function edit()
+    {
+        if (request()->isPost()) {
+            $data = input('post.');
+            if (array_key_exists('status', $data)) {
+                $data['status'] = 1;
+            } else {
+                $data['status'] = 0;
+            }
+
+            $save = db('auth_group')->update($data);
+            if ($save !== false) {
+                $this->success('修改用户组成功！', url('lst'));
+            } else {
+                $this->error('修改用户组失败！');
+            }
+            return;
+        }
+
+        $authGroups = db('auth_group')->find(input('id'));
+        $this->assign('authGroups', $authGroups);
+        return view();
+    }
+
+    public function del()
+    {
+        $del = db('auth_group')->delete(input('id'));
+        if ($del) {
+            $this->success('删除用户组成功！', url('lst'));
+        } else {
+            $this->error('删除用户组失败！');
+        }
     }
 }
