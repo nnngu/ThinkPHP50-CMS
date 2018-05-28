60d6165b5a2658b09dc70f27ec4a6b2efc1985ac
nnngu <654015592@qq.com>
2018-05-29 01:16:43 +0800

    前台 文章内容页 的数据显示

--- application/index/controller/Article.php
+++ application/index/controller/Article.php
@@ -7,6 +7,13 @@ class Article extends Common
 {
     public function index()
     {
+        $artId = input('artid');
+        db('article')->where(['id'=>$artId])->setInc('click');
+        $articles = db('article')->find($artId);
+        $article = new \app\index\model\Article();
+        $hotRes = $article->getHotArticles($articles['cateid']);
+        $this->assign('articles', $articles);
+        $this->assign('hotRes', $hotRes);
         return view('article');
     }
 }
